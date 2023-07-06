87b81b1d36f1f973f6470be9203c8ae724c48e44
Tim Moss <tmoss726@hotmail.com>
2023-07-06 11:42:11 -0400

    Initial commit

---
+++ app/root.jsx
@@ -0,0 +1,64 @@
+import { Outlet, LiveReload, Link, Links, Meta } from '@remix-run/react'
+import globalStylesUrl from '~/styles/globals.css'
+
+export const links = () => [{rel: 'stylesheet', href: globalStylesUrl}]
+
+export const meta = () => {
+  const description = 'A cool blog built with Remix'
+  const keywords = 'remix, react'
+
+  return [{
+    description,
+    keywords,
+  }]
+}
+
+export default function App() {
+  return ( 
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  )
+}
+
+function Document({ children, title }) {
+  return(
+    <html lang='en'>
+      <head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <Meta />
+        <Links />
+        <title>{ title ? title : 'Remix Blog' }</title>
+      </head>
+      <body>
+        {children}
+        {process.env.NODE_ENV === 'development' ? 
+        <LiveReload /> : null}
+      </body>
+    </html>
+  )
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to='/' className='logo'>
+          Remix
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link  to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        {children}
+      </div>
+    </>
+  )
+}
---
+++ app/routes/posts/new.jsx
@@ -0,0 +1,7 @@
+function NewPost() {
+  return (
+    <div>New Post</div>
+  )
+}
+
+export default NewPost
